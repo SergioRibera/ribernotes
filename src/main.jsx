1f484985b30548292667dd1e94280f2e878395e1
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2022-03-27 19:13:47 -0400

    feat: add editor.js dependecies and implementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createReactEditorJS } from "react-editor-js";
+
+import { EDITOR_JS_TOOLS } from "./constants";
+
+const ReactEditorJS = createReactEditorJS()
+
+const App = () => {
+    const handleInit = (editor) => {
+        // editor.save().then(console.log);
+        console.log('Editor.js initialized', editor)
+    }
+    return (
+        <React.StrictMode>
+            <ReactEditorJS
+                tools={EDITOR_JS_TOOLS}
+                defaultValue={{}}
+                onInitialize={handleInit}
+            />
+        </React.StrictMode>
+    )
+}
 
 ReactDOM.render(
-    <React.StrictMode>
-    </React.StrictMode>,
+    <App />,
     document.getElementById('root')
 )
